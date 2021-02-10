4254ae6a7474b5e989ae98d5286945ab812a9c3c
edlinden <eduardo.linden@compliancefiscal.com.br>
2021-02-10 15:45:38 -0300

    Redmine #75495  - Erro na integração de registros F120, incluir o campo chave

--- Patch_2.9.6.2/Packages/catalogar.sql
+++ Patch_2.9.6.2/Packages/catalogar.sql
@@ -4,11 +4,11 @@ prompt Catalogar Packages
 
 prompt -----------------------------------------------------------------------------------
 
-prompt
-@@
+prompt 148a_pk_csf_api_ddo.pks
+@@148a_pk_csf_api_ddo.pks
 show err
-prompt
-@@
+prompt 148b_pk_csf_api_ddo.pkb
+@@148b_pk_csf_api_ddo.pkb
 show err
 
 show err
