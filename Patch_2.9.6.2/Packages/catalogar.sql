1f2b54b98b71a1d19d13dfa9d9f85fbb0a5bf896
edlinden <eduardo.linden@compliancefiscal.com.br>
2021-02-15 16:54:45 -0300

    Redmine #74444 - Pontos de correção no processo de Apuração do IRPJ e CSLL

--- Patch_2.9.6.2/Packages/catalogar.sql
+++ Patch_2.9.6.2/Packages/catalogar.sql
@@ -11,6 +11,13 @@ prompt 148b_pk_csf_api_ddo.pkb
 @@148b_pk_csf_api_ddo.pkb
 show err
 
+prompt 610a_pk_rel_apur_irpj_csll_parc
+@@610a_pk_rel_apur_irpj_csll_parc.pks
+show err
+prompt 610b_pk_rel_apur_irpj_csll_parc
+@@610b_pk_rel_apur_irpj_csll_parc.pkb
+show err
+
 show err
 Prompt Catalogar Objetos Invalidos
 @@rec_obj_invalid.sql
